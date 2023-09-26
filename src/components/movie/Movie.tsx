668bf6b3f92e9d5099536872937dc7085df4194e
SheGaDev <61838855+SheGaDev@users.noreply.github.com>
2023-09-26 17:56:47 +0300

    Update components

--- src/components/movie/Movie.tsx
+++ src/components/movie/Movie.tsx
@@ -1,34 +1,11 @@
-import { useState, useEffect } from 'react';
-import { Link, useLocation, useSearchParams } from 'react-router-dom';
-import Search from '../search/Search';
-import type { MergeSearchMoviesData, SearchMovies } from '@types';
-import { fetch } from '../../services';
+import { Link, useLocation } from 'react-router-dom';
+import type { SearchMovies } from '@types';
 
-const Movies = () => {
-  const [query, setQuery] = useState<string | null>('');
-  const [movies, setMovies] = useState<SearchMovies[] | null>(null);
+const Movies = ({ movies }: { movies: SearchMovies[] }) => {
   const location = useLocation();
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  useEffect(() => {
-    const fetchMovies = async () => {
-      const response = await fetch({ type: 'search', query: query as string });
-      const { results } = response as MergeSearchMoviesData;
-      setMovies(results);
-    };
-    if (query === '' && searchParams) setQuery(searchParams.get('title'));
-    if (query) fetchMovies();
-  }, [query]);
-
-  const onSubmit = (value: string) => {
-    setSearchParams(`?title=${value}`);
-    setMovies(null);
-    setQuery(value);
-  };
 
   return (
     <>
-      <Search onSubmit={onSubmit} />
       {movies && (
         <ul>
           {movies.map((movie) => {
--- src/components/reviews/Reviews.tsx
+++ src/components/reviews/Reviews.tsx
@@ -1,1 +1,22 @@
-export default {};
+import type { ReviewsDataAuthors } from '@types';
+
+const Reviews = ({ reviews }: { reviews: ReviewsDataAuthors[] }) => {
+  return (
+    <ul className='flex flex-col gap-3'>
+      {reviews.map((review) => {
+        return (
+          <li key={review.id} className='flex flex-col gap-1'>
+            <h3>
+              <b>Author: {review.author}</b>
+            </h3>
+            <span>
+              <b>Review:</b> {review.content}
+            </span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default Reviews;
