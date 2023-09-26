cd098d0472103edeab6e76b3909bae2c35690f83
SheGaDev <61838855+SheGaDev@users.noreply.github.com>
2023-09-26 14:04:23 +0000

    Deploying to gh-pages from @ SheGaDev/goit-react-hw-05-movies@69b065d3cc65a05b2d15ab2323367311e4c9fac7 🚀

---
+++ static/js/379.9e29d0a4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[379],{379:function(e,i,t){t.r(i);var s=t(184);i.default=function(){return(0,s.jsx)("div",{children:"Reviews"})}}}]);
+//# sourceMappingURL=379.9e29d0a4.chunk.js.map
