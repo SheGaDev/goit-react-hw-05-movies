d8a68aba1f17dcf1f9a8cbfeee8ecc36db2e3c50
SheGaDev <61838855+SheGaDev@users.noreply.github.com>
2023-09-27 14:05:49 +0300

    Update Pages

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,24 @@
 import { useState, useEffect } from 'react';
 import { fetch } from 'src/services';
 import type { MergeTrendingData, TrendingState } from '@types';
-import Home from '../components/home/Home';
+import MoviesList from '../components/movies-list/MoviesList';
 
 const HomePage = () => {
-  const [trending, setTrending] = useState(null as TrendingState);
+  const [trending, setTrending] = useState<TrendingState | null>(null);
   useEffect(() => {
     fetch({ type: 'trending' }).then((data) => {
       const { results } = data as MergeTrendingData;
       setTrending(results);
     });
   }, []);
-  return <Home trending={trending} />;
+  return (
+    <>
+      <h1>
+        <b className='text-[32px]'>Trending today:</b>
+      </h1>
+      {trending && <MoviesList movies={trending} />}
+    </>
+  );
 };
 
 export default HomePage;
--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -3,38 +3,37 @@ import { useSearchParams } from 'react-router-dom';
 import Search from '../components/search/Search';
 import type { MergeSearchMoviesData, SearchMovies } from '@types';
 import { fetch } from '../services';
-import Movies from '../components/movie/Movie';
+import MoviesList from '../components/movies-list/MoviesList';
 
 const MoviesPage = () => {
-  const [query, setQuery] = useState<string | null>('');
   const [movies, setMovies] = useState<SearchMovies[] | null>(null);
   const [notFound, setNotFound] = useState<boolean>(false);
   const [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(() => {
-    const fetchMovies = async () => {
-      const response = await fetch({ type: 'search', query: query as string });
+    const value = searchParams.get('title');
+    if (!value) return;
+    const fetchMovies = async (query: string) => {
+      const response = await fetch({ type: 'search', query });
       const { results } = response as MergeSearchMoviesData;
       if (!results.length) setNotFound(true);
       else setMovies(results);
     };
-    if (query === '' && searchParams) setQuery(searchParams.get('title'));
-    if (query) fetchMovies();
-  }, [query]);
+    fetchMovies(value);
+  }, [searchParams]);
 
   const onSubmit = (value: string) => {
     if (value !== '') setSearchParams(`?title=${value}`);
     else setSearchParams('');
     setMovies(null);
     setNotFound(false);
-    setQuery(value);
   };
 
   return (
     <div className='flex flex-col gap-3'>
       <Search onSubmit={onSubmit} />
       {notFound && <span>Not Found movies!</span>}
-      {movies && <Movies movies={movies} />}
+      {movies && <MoviesList movies={movies} />}
     </div>
   );
 };
